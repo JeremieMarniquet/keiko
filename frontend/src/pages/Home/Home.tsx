fe7917e17da73976da1e23bcaea3e5d85914b417
Jérémie Marniquet Fabre <jeremie.marniquet@manomano.com>
2019-04-08 18:08:39 +0200

    Add API call

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,19 +1,45 @@
 import * as React from 'react';
 
 import Style from './Home.style';
-import Pokemon from "components/Pokemon/Pokemon";
+import Pokemon from 'components/Pokemon/Pokemon';
+import { makeGetRequest } from 'services/networking/request';
 
-class Home extends React.Component {
+interface PropsType {}
+
+interface StateType {
+  pokemons: { id: number; name: string }[];
+}
+
+class Home extends React.Component<PropsType, StateType> {
+  public readonly state: StateType = {
+    pokemons: [],
+  };
+
   render(): React.ReactNode {
+    console.log(this.state.pokemons);
+
+    if (typeof this.state.pokemons[0] !== 'undefined') {
+      const pokemon = this.state.pokemons[0];
+
+      return (
+        <Style.Intro>
+          <div>Pokédex :</div>
+          <Pokemon pokemon={pokemon} />
+        </Style.Intro>
+      );
+    }
 
     return (
       <Style.Intro>
-          <div>Pokédex :</div>
-          <Pokemon name="Carapuce" id={7}/>
-          <Pokemon name="Carabaffe" id={8}/>
-          <Pokemon name="Tortank" id={9}/>
+        <div>Pokédex :</div>
       </Style.Intro>
     );
+  }
+
+  componentDidMount() {
+    makeGetRequest('/pokemon/').then(response => {
+      this.setState({ pokemons: response.body });
+    });
   }
 }
 
