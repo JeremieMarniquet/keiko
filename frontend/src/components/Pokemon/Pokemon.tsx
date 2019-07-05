86e2c9d54b1c2c28d275fdcfc9c4fc3c2e39bcb5
Jérémie Marniquet Fabre <jeremie.marniquet@manomano.com>
2019-07-05 17:19:46 +0200

    Add style to pokedex

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import { Box, BoxContainer, TextBox } from './Pokemon.style';
 
 interface Props {
-  pokemon: { id: number; name: string };
+  pokemon: { id: number; name: string; weight: number; height: number };
 }
 
 class Pokemon extends React.Component<Props> {
@@ -12,9 +14,17 @@ class Pokemon extends React.Component<Props> {
 
     return (
       <>
-        <div>{this.props.pokemon.id}</div>
-        <div>{this.props.pokemon.name}</div>
-        <img src={url} width="50" height="50" />
+        <BoxContainer>
+          <Box>
+            <TextBox>{this.props.pokemon.name}</TextBox>
+            <img src={url} width="80" height="80" />
+            <TextBox>
+              <div>Id: {this.props.pokemon.id}</div>
+              <div>Weight: {this.props.pokemon.weight / 10} kg</div>
+              <div>Height: {this.props.pokemon.height * 10} cm</div>
+            </TextBox>
+          </Box>
+        </BoxContainer>
       </>
     );
   }
