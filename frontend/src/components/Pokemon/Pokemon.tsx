aa1bf6c03a072a5203ea14d0923d668df5e041ca
Maxence Lecanu <lecanu.maxence@gmail.com>
2019-10-01 15:35:01 +0200

    Pokemon component is created using a hook + the pokemon shows his back on click

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 
 import Style from './Pokemon.style';
 import { FormattedMessage } from 'react-intl';
@@ -10,25 +10,34 @@ interface Props {
   height: number;
 }
 
-class Pokemon extends React.Component<Props> {
-  render(): React.ReactNode {
-    const imageURl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.pokedexId}.png`;
-    return (
-      <Style.Pokemon>
-        <p>{this.props.name}</p>
-        <img src={imageURl} alt={this.props.name} />
-        <p>
-          <FormattedMessage id="pokemon.id" /> : {this.props.pokedexId}
-        </p>
-        <p>
-          <FormattedMessage id="pokemon.weight" /> : {this.props.weight}kg
-        </p>
-        <p>
-          <FormattedMessage id="pokemon.height" /> : {this.props.height}cm
-        </p>
-      </Style.Pokemon>
-    );
-  }
-}
+var Pokemon = (props: Props) => {
+  const [showsBack, setShowsBack] = useState(false);
+  let imageUrl: string = showsBack
+    ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${props.pokedexId}.png`
+    : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.pokedexId}.png`;
+  return (
+    <Style.Pokemon onClick={() => setShowsBack(!showsBack)}>
+      <p>{props.name}</p>
+      <img src={imageUrl} alt={props.name} />
+      <p>
+        <FormattedMessage id="pokemon.id" /> : {props.pokedexId}
+      </p>
+      <p>
+        <FormattedMessage id="pokemon.weight" /> : {props.weight}kg
+      </p>
+      <p>
+        <FormattedMessage id="pokemon.height" /> : {props.height}cm
+      </p>
+    </Style.Pokemon>
+  );
+};
+
+// class Pokemon extends React.Component<Props> {
+//   render(): React.ReactNode {
+//     const imageURl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.pokedexId}.png`;
+//     return (
+//     );
+//   }
+// }
 
 export default Pokemon;
