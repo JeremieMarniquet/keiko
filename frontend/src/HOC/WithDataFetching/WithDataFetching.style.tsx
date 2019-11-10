a2fbee8fa3b961d695a4add97395e8b4e9f00c22
Maxence Lecanu <lecanu.maxence@gmail.com>
2019-11-10 14:11:42 +0100

    Style for the loader and error messages in the WithDataFetching component

---
+++ frontend/src/HOC/WithDataFetching/WithDataFetching.style.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export default {
+  Wrapper: styled.div`
+    width: 100%;
+    margin-top: 2em;
+    display: flex;
+  `,
+  ErrorMessage: styled.p`
+    margin: auto;
+    color: red;
+    text-align: center;
+  `,
+  Loader: styled.img`
+    margin: auto;
+    width: 200px;
+    height: auto;
+  `,
+};
