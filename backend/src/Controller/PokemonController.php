d28862ef53094431306e709e60bf8778e74991a3
Maxence Lecanu <lecanu.maxence@gmail.com>
2019-11-10 14:58:13 +0100

    The controller's route is registered using an annotation

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PokemonController
 {
+    /**
+     * @Route("/api/pokemon")
+     */
     public function hello()
     {
         return new Response(
