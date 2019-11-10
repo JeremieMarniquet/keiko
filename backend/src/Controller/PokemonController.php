a6b6611364e80f1316da99955272d6c733f552fc
Maxence Lecanu <lecanu.maxence@gmail.com>
2019-11-10 15:44:12 +0100

    Get returns a hard-coded pokemon, post /api/pokemon returns hello world

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Pokemon;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class PokemonController
 {
-    /**
-     * @Route("/api/pokemon")
-     */
-    public function hello()
+    private $normalizer;
+
+    public function __construct(NormalizerInterface $normalizer)
     {
-        return new Response(
-            'Hello world'
-        );
+        $this->normalizer = $normalizer;
+    }
+
+    /**
+     * @Route("/api/pokemon", methods={"GET"})
+     */
+    public function get()
+    {
+        $pokemon = new Pokemon();
+        $pokemon->setName('Pikachu');
+        $pokemon->setHeight(10);
+        $pokemon->setWeight(10);
+
+        $content = $this->normalizer->normalize($pokemon, 'json');
+
+        return new JsonResponse($content);
+    }
+
+    /**
+     * @Route("api/pokemon", methods={"POST"})
+     */
+    public function post()
+    {
+        return new Response('Hello World');
     }
 }
