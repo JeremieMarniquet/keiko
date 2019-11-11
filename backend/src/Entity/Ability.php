0edd14c6fe25c08782e77dc4d92037b1967b7d7c
Maxence Lecanu <lecanu.maxence@gmail.com>
2019-11-11 11:07:25 +0100

    Refacto with API Platform + CRUD for abilities

---
+++ backend/src/Entity/Ability.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource
+ * @ORM\Table(name="ability")
+ * @ORM\Entity()
+ */
+class Ability
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\NotNull
+     */
+    private $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+}
